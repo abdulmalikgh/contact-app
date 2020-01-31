7c90a54448041dd03932202b562fccf83446d38c
abdulmalikgh <50835233+abdulmalikgh@users.noreply.github.com>
2020-01-31 10:11:27 -0800

    CreateContact completed

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -2,27 +2,34 @@ import React, {Component} from 'react';
 import './App.css'; 
 import {Link} from 'react-router-dom';
 import ImageInput from './ImageInput';
+import SerializeForm from 'form-serialize';
 
 class CreateContact extends Component{
-
+   handleForm = (e) => {
+       e.preventDefault();
+       const values = SerializeForm(e.target, {hash:true})
+       if(this.props.onCreateContact) {
+           this.props.onCreateContact(values)
+       }
+   }
     render(){
         return (
          <div>
           <Link to='/' className='close-create-contact'>
               Close
           </Link>
-          <form className='create-contact-form'>
+          <form className='create-contact-form' onSubmit={this.handleForm}>
               <ImageInput 
               className='create-contact-avatar-input'
               name='avatarURL'
               maxHeight={64}
               />
-          </form>
-          <div className='create-contact-details'>
+              <div className='create-contact-details'>
               <input type='text' name='name' placeholder='Name' />
               <input type='text' name='handle' placeholder='handle' />
               <button>Add Contact</button>
           </div>
+          </form>
         </div>
         )
     }
